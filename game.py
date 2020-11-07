e2115493ba230f199fc49b1de6fdc629b83147c4
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-07 12:49:50 +0100

    Add choosing marker by player 1

--- game.py
+++ game.py
@@ -9,14 +9,28 @@ def clear_screen():
 
 
 def display_board(board):
-    print("| " + board[7] + " | " + board[8] + " | " + board[9] + " |")
-    print("| " + board[4] + " | " + board[5] + " | " + board[6] + " |")
-    print("| " + board[1] + " | " + board[2] + " | " + board[3] + " |")
     clear_screen()
     print("| " + board[7] + " | " + board[8] + " | " + board[9] + " |")
     print("| " + board[4] + " | " + board[5] + " | " + board[6] + " |")
     print("| " + board[1] + " | " + board[2] + " | " + board[3] + " |")
 
 
+def player_input():
+    """
+    OUTPUT = (Player 1 marker, Player 2 marker)
+    """
+    marker = ""
+    while marker != "X" and marker != "O":
+        marker = input("Player1: Choose X or O: ").upper()
+    if marker == "X":
+        return ("X", "O")
+    else:
+        return ("O", "X")
+
+
 test_board = ["#", "X", "O", "X", "O", "X", "O", "X", "O", "X"]
+
+player1_marker, player2_marker = player_input()
 display_board(test_board)
+
+print(player1_marker, player2_marker)
