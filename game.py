2a9e867010a01c459b81a8d34f8a905f78b5ba87
Jakub Ściślewski <32902950+JScislewski@users.noreply.github.com>
2020-11-07 12:41:12 +0100

    Add clearing console

--- game.py
+++ game.py
@@ -1,4 +1,18 @@
+from os import system, name
+
+
+def clear_screen():
+    if name == "nt":
+        _ = system("cls")
+    else:
+        _ = system("clear")
+
+
 def display_board(board):
+    print("| " + board[7] + " | " + board[8] + " | " + board[9] + " |")
+    print("| " + board[4] + " | " + board[5] + " | " + board[6] + " |")
+    print("| " + board[1] + " | " + board[2] + " | " + board[3] + " |")
+    clear_screen()
     print("| " + board[7] + " | " + board[8] + " | " + board[9] + " |")
     print("| " + board[4] + " | " + board[5] + " | " + board[6] + " |")
     print("| " + board[1] + " | " + board[2] + " | " + board[3] + " |")
